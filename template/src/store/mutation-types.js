cb6c6bac797de88554d9cc4927abdb7328c51012
Leo <leo@xuebin.me>
2018-04-16 10:17:15 +0800

    ⚡️Mutation-types 去中心化

--- template/src/store/mutation-types.js
+++ template/src/store/mutation-types.js
@@ -2,11 +2,33 @@
  * @Author: Leo - [xuebin.me]
  * @Date: 2017-12-29 13:59:41
  * @Last Modified by: Leo
- * @Last Modified time: 2018-03-23 13:37:09
+ * @Last Modified time: 2018-04-16 09:58:18
  */
 
-export const sys = { }
-
-export const base = {
-  SET_VERSION: '设置版本号'
+export default {
+  sys: {
+    mutations: {
+      SET_VERSION: '设置版本号'
+    },
+    actions: {}
+  },
+  ...(r => {
+    // 去中心化
+    let sourceMap = {}
+    // eslint-disable-next-line
+    let res = r.keys().map(key => {
+      let rKey = r(key)
+      let newKey = key.replace(/^\.\/modules\/(.*)\.js$/g, '$1')
+      let namespaced = rKey.default.namespaced ? `${newKey}/` : ''
+      sourceMap[newKey] = {}
+      for (let key in rKey.default.mutations) {
+        sourceMap[newKey]['mutations'] = `${namespaced}${key}`
+      }
+      for (let key in rKey.default.actions) {
+        sourceMap[newKey]['actions'] = `${namespaced}${key}`
+      }
+      return rKey.default
+    })
+    return sourceMap
+  })(require.context('./', true, /^\.\/modules\/\w+\.js$/))
 }
